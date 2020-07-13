29f7e908644d5486145bbc5d33cca7622820df59
Isaac Powell <powelli91@outlook.com>
2020-07-12 20:14:40 -0700

    trying to use open rasta in the sandbox app. i think the project needs to target netstandard2.0

---
+++ OpenRastaSandbox/SandboxApp.cs
@@ -0,0 +1,19 @@
+﻿using OpenRasta.Configuration;
+using OpenRasta.DI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenRastaSandbox
+{
+    public class SandboxApp : IConfigurationSource, IDependencyResolverAccessor
+    {
+        public IDependencyResolver Resolver => throw new NotImplementedException();
+
+        public void Configure()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
