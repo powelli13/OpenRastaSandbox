9477d5525dc01ef1e1c6e49625ec36a7ed98057f
Isaac Powell <powelli91@outlook.com>
2020-07-14 20:59:19 -0700

    added a bunch of assemblies. got the app to run and return 406s and 500s. not sure how to configure the resources.

--- OpenRastaSandbox/SandboxApp.cs
+++ OpenRastaSandbox/SandboxApp.cs
@@ -1,5 +1,7 @@
-﻿using OpenRasta.Configuration;
+﻿using OpenRasta.Codecs.Newtonsoft.Json;
+using OpenRasta.Configuration;
 using OpenRasta.DI;
+using OpenRasta.DI.Windsor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +11,48 @@ namespace OpenRastaSandbox
 {
     public class SandboxApp : IConfigurationSource, IDependencyResolverAccessor
     {
-        public IDependencyResolver Resolver => throw new NotImplementedException();
+        public IDependencyResolver Resolver { get; } = new WindsorDependencyResolver();
+
+        class PlanetData
+        {
+            public string Name { get; set; }
+
+            public class Handler
+            {
+                public PlanetData Get() => 
+                    new PlanetData() { Name = "Earth" };
+            }
+        }
+
+        class RootResponse
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public class RootHandler
+            {
+                public RootResponse Get() =>
+                    new RootResponse() { Name = "Hi there", Age = 100 };
+            }
+        }
 
         public void Configure()
         {
-            throw new NotImplementedException();
+            ResourceSpace.Has
+                .ResourcesOfType<RootResponse>()
+                .AtUri("/")
+                .Named("root")
+                .HandledBy<RootResponse.RootHandler>()
+                .TranscodedBy<NewtonsoftJsonCodec>()
+                .ForMediaType("application/json");
+
+            ResourceSpace.Has
+                .ResourcesOfType<PlanetData>()
+                .AtUri("/planets")
+                .Named("planets")
+                .HandledBy<PlanetData.Handler>()
+                .TranscodedBy<NewtonsoftJsonCodec>()
+                .ForMediaType("application/json");
         }
     }
 }
--- OpenRastaSandbox/Startup.cs
+++ OpenRastaSandbox/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenRasta.Hosting.AspNet;
+using OpenRasta.Hosting.AspNetCore;
 
 namespace OpenRastaSandbox
 {
@@ -25,31 +25,31 @@ namespace OpenRastaSandbox
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
 
-            //app.UseOpenRasta(new SandboxApp());
+            app.UseOpenRasta(new SandboxApp());
         }
     }
 }
